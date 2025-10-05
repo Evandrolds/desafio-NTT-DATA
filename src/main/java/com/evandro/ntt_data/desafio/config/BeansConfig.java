1261a7902e48b1dcda4625a4ea19bb6159890144
Evandrolds <evandro.lds.ds@outlook.com>
2025-10-05 15:57:24 -0300

    refactor: BeansConfig

--- src/main/java/com/evandro/ntt_data/desafio/config/BeansConfig.java
+++ src/main/java/com/evandro/ntt_data/desafio/config/BeansConfig.java
@@ -1,9 +1,12 @@
 package com.evandro.ntt_data.desafio.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class BeansConfig {
 
     @Bean
@@ -11,7 +14,9 @@ public class BeansConfig {
         return new ObjectMapper();
     }
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .defaultHeader("User-Agent", "sua-app/1.0 (email@dominio.com)")
+                .build();
     }
 }
